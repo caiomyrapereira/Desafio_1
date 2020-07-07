d0c56414d81ed6273beaa83c43d3f29f2f236452
caio myra pereira <caiomyrapereira@hotmail.com>
2020-07-07 16:00:36 -0300

    Desafio_1 finalizando.

--- main.js
+++ main.js
@@ -1,90 +1,89 @@
 (function Main(){
-    var clients = [];
 
-    async function FetchCliente()
-    {
-        clients = [
-            {
-                name: "Kauan Pereira Rocha",
-                telefone: "(81) 9479-5548"
+    const app = new Vue({
+        el: '#app',
+        data:{
+            clients : [
+                {
+                    name: "Kauan Pereira Rocha",
+                    telefone: "(81) 9479-5548"
+                },
+                {
+                    name: "Antônio Oliveira Cunha",
+                    telefone: "(73) 3626-4953"
+                },
+                {
+                    name: "Paulo Ferreira Sousa",
+                    telefone: "(61) 5620-5278"
+                },
+                {
+                    name: "Beatrice Silva Gomes",
+                    telefone: "(81) 9362-3427"
+                },
+                {
+                    name: "Gabriela Oliveira Sousa",
+                    telefone: "(19) 6770-3263"
+                },
+                {
+                    name: "Alex Oliveira Correia",
+                    telefone: "(32) 6974-7896"
+                },
+                {
+                    name: "Breno Azevedo Martins",
+                    telefone: "(91) 7167-8651"
+                },
+                {
+                    name: "Giovanna Rocha Barros",
+                    telefone: "(21) 6495-8033"
+                },
+                {
+                    name: "Daniel Sousa Carvalho",
+                    telefone: "(81) 9852-4166"
+                },
+                {
+                    name: "Luis Rodrigues Barros",
+                    telefone: "(85) 8946-9329"
+                },
+                {
+                    name: "Felipe Fernandes Almeida",
+                    telefone: "(21) 7167-9927"
+                },
+                {
+                    name: "Lavinia Ribeiro Ferreira",
+                    telefone: "(35) 6497-5127"
+                },
+                {
+                    name: "Danilo Araujo Costa",
+                    telefone: "(91) 5514-7876"
+                },
+                {
+                    name: "Leila Fernandes Cunha",
+                    telefone: "(11) 5356-3574"
+                },
+                {
+                    name: "Livia Silva Ribeiro",
+                    telefone: "(47) 4241-8712"
+                }
+            ],
+            
+            fullName:'',
+            message:'',
+        },
+
+        methods:{
+            filterClient(elem){
+                const regex = RegExp('^'+this.removeSpace(this.fullName),'gim');
+                return regex.test(elem.name) && this.removeSpace(this.fullName);     
             },
-            {
-                name: "Antônio Oliveira Cunha",
-                telefone: "(73) 3626-4953"
+            removeSpace(string){
+                const newString = string.replace(/\s{2,}/g, ' ').trim();
+                return newString;
             },
-            {
-                name: "Paulo Ferreira Sousa",
-                telefone: "(61) 5620-5278"
-            },
-            {
-                name: "Beatrice Silva Gomes",
-                telefone: "(81) 9362-3427"
-            },
-            {
-                name: "Gabriela Oliveira Sousa",
-                telefone: "(19) 6770-3263"
-            },
-            {
-                name: "Alex Oliveira Correia",
-                telefone: "(32) 6974-7896"
-            },
-            {
-                name: "Breno Azevedo Martins",
-                telefone: "(91) 7167-8651"
-            },
-            {
-                name: "Giovanna Rocha Barros",
-                telefone: "(21) 6495-8033"
-            },
-            {
-                name: "Daniel Sousa Carvalho",
-                telefone: "(81) 9852-4166"
-            },
-            {
-                name: "Luis Rodrigues Barros",
-                telefone: "(85) 8946-9329"
-            },
-            {
-                name: "Felipe Fernandes Almeida",
-                telefone: "(21) 7167-9927"
-            },
-            {
-                name: "Lavinia Ribeiro Ferreira",
-                telefone: "(35) 6497-5127"
-            },
-            {
-                name: "Danilo Araujo Costa",
-                telefone: "(91) 5514-7876"
-            },
-            {
-                name: "Leila Fernandes Cunha",
-                telefone: "(11) 5356-3574"
-            },
-            {
-                name: "Livia Silva Ribeiro",
-                telefone: "(47) 4241-8712"
+            selectClient(index){
+                this.message = this.clients[index].name+' - '+this.clients[index].telefone;
             }
-        ]
-    }
-
-    function Update()
-    {
-        var $list = document.getElementById('list');
-        var aux = "";
-        for(var i = 0; i < clients.length; i++)
-        {
-            aux += `<li>${clients[i].name}</li>`;
-        }
-        $list.innerHTML = aux;
-    }
-
-    async function Start()
-    {
-        await FetchCliente();
-        Update();
-    }
-
-    Start();
+        },      
+    });
 
 
     window.Main = {
